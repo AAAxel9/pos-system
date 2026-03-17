ee86185a8da9c58ea9eac3e59c3b395008676241
axelzavaleta <axel.zavaleta.h@gmail.com>
2026-03-16 23:48:23 -0300

    feat: implementar verificacion de conexion a BD previo a levantar el servidor

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,17 @@
 import app from "./server/app.js";
 import "dotenv/config";
+import pool from "./db/config.db.js";
 
-const PORT = process.env.PORT;
+const SERVER_PORT = process.env.SERVER_PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+await pool.query("select now()").then((result) => {
+  console.log("--------------------------------");
+  console.log("Database successfully connected:", result.rows[0].now);
+  console.log("--------------------------------");
+  
+  app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on http://localhost:${SERVER_PORT}`);
+  })
+}).catch((error) => {
+  console.error("Database connection failed:", error);
 });
